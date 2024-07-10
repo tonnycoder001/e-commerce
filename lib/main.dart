a41116eab103eb0f190ff1d266635774beaf8cc0
tonnycoder001 <tonnykahihia@gmail.com>
2024-07-10 09:53:01 +0300

    Add items to cart

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:provider/provider.dart';
+
+import 'models/cart.dart';
+import 'pages/intro_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Cart(),
+      builder: (context, child) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroPage(),
+      ),
+    );
+  }
+}
